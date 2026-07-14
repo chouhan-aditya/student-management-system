a2ba77becb5621706db3eaa5e7926215bc11ad69
chouhan-aditya <adityachouhan2806@gmail.com>
2026-07-14 16:33:02 +0530

    Added Render deployment settings

--- student_project/settings.py
+++ student_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q$y)71ek6hx%6b&32300x2oz+1g)_--*5nmw_bcxdpq_+y=(=@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
